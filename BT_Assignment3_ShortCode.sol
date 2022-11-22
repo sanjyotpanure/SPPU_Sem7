543d40f14cb241c88bd750ae5ae99e9597ea20bc
Sanjyot Panure <sanjyotpanure37@gmail.com>
2022-11-22 16:51:30 +0530

    Create BT_Assignment3_ShortCode.sol

---
+++ BT_Assignment3_ShortCode.sol
@@ -0,0 +1,50 @@
+pragma solidity ^0.5.8;
+
+contract Banking{
+
+    uint balance;
+
+    constructor() public{
+        balance = 100;
+    }
+
+    function getBalance() view public returns(uint){
+        return balance;
+    }
+
+    function withdraw(uint amt) public returns(uint remainingBal){
+        if (amt <= balance){
+            balance = balance - amt;
+        }
+        return balance;
+    }
+
+    function deposit(uint amt) public returns(uint remainingBal){
+        balance = balance + amt;
+        return balance;
+    }
+}
+
+//*************** Alternative Way *************
+//***************** Program 2 *****************
+pragma solidity ^0.6;
+contract banking
+{
+    mapping(address=>uint) private user_account;
+    function deposit(uint amount) public payable returns(string memory)
+    {
+        user_account[msg.sender] += amount;
+        return "Deposited Successfully";
+    }
+    function withdraw(uint amount) public payable returns(string memory)
+    {
+        require(user_account[msg.sender]>amount,"Insufficient Balance");
+        require(amount>0,"Amount should be more than zero");
+        user_account[msg.sender]=user_account[msg.sender]-amount;
+        return "Withdrawl Successful";
+    }
+    function user_balance() public view returns(uint)
+    {
+        return user_account[msg.sender];
+    }
+}
